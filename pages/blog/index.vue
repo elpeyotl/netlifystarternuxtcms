da805c2206a7239a28396f9674829d154cf602d3
elpeyotl <ch.perez@mac.com>
2020-05-15 00:20:07 +0200

    added blog

---
+++ pages/blog/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+    <ul>
+        <nuxt-link :to="`${blog.slug}`" v-for="blog in blogPosts" :key="blog.slug">{{blog.title}}</nuxt-link>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts;
+    },
+  },
+  methods: {
+      goToDetail(slug) {
+          this.$router.push(`${slug}`)
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
